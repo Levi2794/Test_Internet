9ab328c9f6396f54ad617b0502d089340171a668
unknown <traceur.rick@gmail.com>
2021-06-10 18:20:12 -0500

    Lineas para la optener el valor de subida

--- prueba.py
+++ prueba.py
@@ -2,21 +2,18 @@
 #Importa la libreria speedtest
 import speedtest
 
+st = speedtest.Speedtest()
 
 def Subida():
-    pass
+    #Velocidad de subida
+    dw = st.download()/1000000
+    print('Subida: {:.2f}'.format(dw) + ' Mb/s')
 
 def Descarga():
     pass
 
 def Ping():
     pass
-
-st = speedtest.Speedtest()
-
-#Velocidad de subida
-dw = st.download()/1000000
-print('Subida: {:.2f}'.format(dw) + ' Mb/s')
 
 #Velocidad de descarga
 up = st.upload()/1000000
